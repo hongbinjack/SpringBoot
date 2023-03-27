512e3bf90e62a57e0ceb18bc0bc7e4169a8bc102
hongbin <2623484066@qq.com>
2023-03-27 19:48:55 +0800

    springboot整合mongodb

---
+++ springboot_17_mongodb/src/main/java/com/hb/domain/Book.java
@@ -0,0 +1,14 @@
+package com.hb.domain;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private Integer id;
+    private String type;
+    private String name;
+    private String description;
+
+    }
+
--- springboot_17_mongodb/src/test/java/com/hb/Springboot17MongodbApplicationTests.java
+++ springboot_17_mongodb/src/test/java/com/hb/Springboot17MongodbApplicationTests.java
@@ -1,13 +1,33 @@
 package com.hb;
 
+import com.hb.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.List;
 
 @SpringBootTest
 class Springboot17MongodbApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+    @Test
+	void testMongodb(){
+		Book book = new Book();
+		book.setId(2);
+		book.setName("springboot88");
+		book.setDescription("springboot99");
+        book.setType("skill66");
+		mongoTemplate.save(book);
 	}
+
+      @Test
+	void findAll(){
+		  List<Book> all = mongoTemplate.findAll(Book.class);
+		  System.out.println(all);
+	  }
 
 }
